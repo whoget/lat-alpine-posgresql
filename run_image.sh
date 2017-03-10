93a17d0a6bee6c98df46c2561d6f8ffa5ad0e1f4
pete <whoget@gmail.com>
2017-03-10 22:01:04 +0700

    update run_image

--- run_image.sh
+++ run_image.sh
@@ -1,1 +1,1 @@
-docker run -d --name alpine-posgre -p 7777:5432 -e POSTGRES_PASSWORD=admin@postgresql -v /opt/postgresql/data:/var/lib/postgresql/data -e POSTGRES_LISTEN_ADDRESSES=*  whoget/lat-alpine-postgresql:9.5
+docker run -d --name alpine-posgre -p 7777:5432 -e POSTGRES_PASSWORD=admin@postgresql -v /opt/postgresql/data:/var/lib/postgresql/data -e POSTGRES_LISTEN_ADDRESSES=*  whoget/alpine-postgresql:9.5
