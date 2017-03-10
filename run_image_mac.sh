db477c667d2407a71e93df1b780c3a9a5649ed32
pete <whoget@gmail.com>
2017-03-10 22:15:55 +0700

    update run_image_mac

---
+++ run_image_mac.sh
@@ -0,0 +1,1 @@
+docker run -d --name alpine-posgre -p 7777:5432 -e POSTGRES_PASSWORD=admin@postgresql -v ~/DOCKER_DATA/postgresql/data:/var/lib/postgresql/data -e POSTGRES_LISTEN_ADDRESSES=*  whoget/alpine-postgresql:9.5
